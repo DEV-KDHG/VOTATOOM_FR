4e213c65cb157e88c3d8c3b7ad2759eeb733e036
rafapcjs <rafaelcorredorgambin1@gmail.com>
2024-04-22 16:24:01 -0500

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Forms from './view/formulario/Login'
+import ListStudentOfObsevers from './view/ListStudentsOf/ListStudentOfObsevers'
 
-import Personero from './view/formulario/Personero'
-import TarjetCandidates from './shared/tarjetCandidates/TarjetCandidates'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<TarjetCandidates/>
+<ListStudentOfObsevers/>
   </React.StrictMode>,
 )
